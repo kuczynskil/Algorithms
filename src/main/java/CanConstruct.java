22bdf27a8d413531d4646039f85dfcb3a2585e8c
kuczynskil <l.kuczynski95@gmail.com>
2021-02-09 18:07:53 +0100

    Update canConstruct soluton
    Added memoization

--- src/main/java/CanConstruct.java
+++ src/main/java/CanConstruct.java
@@ -1,20 +1,30 @@
 package main.java;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CanConstruct {
 
-    public static boolean canConstruct(String target, String[] elements) {
+    public static boolean canConstruct(String target, String[] elements, Map<String, Boolean> memo) {
+        if (memo.containsKey(target)) return memo.get(target);
         if (target.isEmpty()) return true;
         for (String element : elements) {
             if (target.indexOf(element) == 0) {
                 String remainder = target.substring(element.length());
-                if (canConstruct(remainder, elements)) return true;
+                if (canConstruct(remainder, elements, memo)) {
+                    memo.put(target, true);
+                    return true;
+                }
             }
         }
+        memo.put(target, false);
         return false;
     }
 
     public static void main(String[] args) {
-        System.out.println(canConstruct("skateboard", new String[] {"ab", "abc", "bc", "skateboard", "111"}));
+        Map<String, Boolean> map = new HashMap<>();
+        System.out.println(canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", new String[] {"e", "abeeec", "eee",
+                "eeeeeeeeee", "ee"}, map));
     }
 }
 
