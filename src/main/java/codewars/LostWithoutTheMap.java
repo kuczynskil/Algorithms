3ac6e028f3e29e115384151f9de4033f69ebe686
kuczynskil <l.kuczynski95@gmail.com>
2021-03-15 20:32:04 +0100

    Add LostWithoutTheMap solution

---
+++ src/main/java/codewars/LostWithoutTheMap.java
@@ -0,0 +1,18 @@
+package main.java.codewars;
+
+import java.util.Arrays;
+
+public class LostWithoutTheMap {
+
+//    Given an array of integers, return a new array with each value doubled.
+//
+//For example:
+//
+//[1, 2, 3] --> [2, 4, 6]
+//
+//For the beginner, try to use the map method - it comes in very handy quite a lot so is a good one to know.
+
+    public static int[] map(int[] arr) {
+        return Arrays.stream(arr).map(el -> el * 2).toArray();
+    }
+}
