4c73aa626ec4fa29e71c5ed40858585954b52c36
kuczynskil <l.kuczynski95@gmail.com>
2021-03-15 20:15:34 +0100

    Add TwiceAsOld solution

---
+++ src/main/java/codewars/TwiceAsOld.java
@@ -0,0 +1,14 @@
+package main.java.codewars;
+
+public class TwiceAsOld {
+
+//    Your function takes two arguments:
+//
+//current father's age (years)
+//current age of his son (years)
+//Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+
+    public static int TwiceAsOld(int dadYears, int sonYears){
+        return Math.abs(dadYears - 2 * sonYears);
+    }
+}
