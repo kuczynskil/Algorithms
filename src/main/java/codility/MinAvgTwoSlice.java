61127b02416a24f63c0fe6f95ae8d69f43751161
kuczynskil <l.kuczynski95@gmail.com>
2021-04-02 12:34:37 +0200

    Add MinAvgTwoSlice solution

---
+++ src/main/java/codility/MinAvgTwoSlice.java
@@ -0,0 +1,80 @@
+package main.java.codility;
+
+public class MinAvgTwoSlice {
+
+//    A non-empty array A consisting of N integers is given.
+//    A pair of integers (P, Q), such that 0 ≤ P < Q < N, is
+//    called a slice of array A (notice that the slice contains
+//    at least two elements). The average of a slice (P, Q) is t
+//    he sum of A[P] + A[P + 1] + ... + A[Q] divided by the length
+//    of the slice. To be precise, the average equals (A[P] + A[P + 1] + ... + A[Q]) / (Q − P + 1).
+//
+//For example, array A such that:
+//
+//    A[0] = 4
+//    A[1] = 2
+//    A[2] = 2
+//    A[3] = 5
+//    A[4] = 1
+//    A[5] = 5
+//    A[6] = 8
+//contains the following example slices:
+//
+//slice (1, 2), whose average is (2 + 2) / 2 = 2;
+//slice (3, 4), whose average is (5 + 1) / 2 = 3;
+//slice (1, 4), whose average is (2 + 2 + 5 + 1) / 4 = 2.5.
+//The goal is to find the starting position of a slice whose average is minimal.
+//
+//Write a function:
+//
+//class Solution { public int solution(int[] A); }
+//
+//that, given a non-empty array A consisting of N integers, returns
+// the starting position of the slice with the minimal average. If
+// there is more than one slice with a minimal average, you should return the smallest starting position of such a slice.
+//
+//For example, given array A such that:
+//
+//    A[0] = 4
+//    A[1] = 2
+//    A[2] = 2
+//    A[3] = 5
+//    A[4] = 1
+//    A[5] = 5
+//    A[6] = 8
+//the function should return 1, as explained above.
+//
+//Write an efficient algorithm for the following assumptions:
+//
+//N is an integer within the range [2..100,000];
+//each element of array A is an integer within the range [−10,000..10,000].
+
+    public int solution(int[] A) {
+        int sumOfTwo, sumOfThree = 0;
+        double minAverage = Double.MAX_VALUE;
+        int indexRes = 0;
+
+        for (int i = 0; i < A.length - 2; i++) {
+            sumOfTwo = A[i] + A[i + 1];
+            if (sumOfTwo / 2.0 < minAverage) {
+                minAverage = sumOfTwo / 2.0;
+                indexRes = i;
+            }
+
+            sumOfThree = sumOfTwo + A[i + 2];
+            if (sumOfThree / 3.0 < minAverage) {
+                minAverage = sumOfThree / 3.0;
+                indexRes = i;
+            }
+        }
+
+        sumOfTwo = A[A.length - 1] + A[A.length - 2];
+        if (sumOfTwo / 2.0 < minAverage) indexRes = A.length - 2;
+
+        return indexRes;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new MinAvgTwoSlice().solution(new int[] {4, 2, 2, 5, 1, 5, 8}));
+    }
+}
